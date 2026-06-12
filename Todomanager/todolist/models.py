35ac754506782dad61605fa67a3681ebe9b7b2e8
Sohel Mallik <malliksohel582@gmail.com>
2026-06-12 11:21:57 +0530

    Migrate on DB and connect with  Admin Panel

--- Todomanager/todolist/models.py
+++ Todomanager/todolist/models.py
@@ -6,3 +6,5 @@ class Task(models.Model):#Tell it is model
     task= models.CharField(max_length=200) #Task is a character field with max length of 200
     is_completed= models.BooleanField(default=False) #is_completed is a boolean field with default value of False
 
+    def __str__(self):
+         return f"{self.task}"
