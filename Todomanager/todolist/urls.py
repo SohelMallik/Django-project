4dcd217a5a54c42163e8f6e4c0b8608afb1687ab
Sohel Mallik <malliksohel582@gmail.com>
2026-06-17 21:40:44 +0530

    Rename path nme

--- Todomanager/todolist/urls.py
+++ Todomanager/todolist/urls.py
@@ -3,7 +3,7 @@ from . import views
 #Connect Urls with Views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('contact/', views.contact, name='contact'),
+    path('contactus/', views.contact, name='contact'),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('todolist/', views.todolist, name='todolist'),
 
