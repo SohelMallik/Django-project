aa703ba62e02a7aae14695d0db9a992d49365f14
Sohel Mallik <malliksohel582@gmail.com>
2026-07-11 00:02:47 +0530

    todolist/urls.py

--- Todomanager/todolist/urls.py
+++ Todomanager/todolist/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('contactus/', views.contact, name='contact'),
     path('about/', views.aboutus, name='aboutus'),
     path('todolist/', views.todolist, name='todolist'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+
 
 ]
