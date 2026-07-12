2b41a2aad2590b458ea005e181ea1fda7ef0e4ad
Sohel Mallik <malliksohel582@gmail.com>
2026-07-12 09:38:51 +0530

    Create Url For Edit

--- Todomanager/todolist/urls.py
+++ Todomanager/todolist/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('about/', views.aboutus, name='aboutus'),
     path('todolist/', views.todolist, name='todolist'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
+
 
 
 ]
